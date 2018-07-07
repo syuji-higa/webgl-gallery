e2d24c9674592530cfece2aa57d138b866968f0f
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2018-07-07 14:17:19 +0900

    add WebIndex&Quantum

--- build.js
+++ build.js
@@ -1,58 +1,67 @@
-const { writeFile, copyFile } = require('fs');
 const { join } = require('path');
-const pug = require('pug');
 const {
   FuseBox,
   BabelPlugin,
+  WebIndexPlugin,
+  QuantumPlugin,
 } = require('fuse-box');
 
 const dir = process.argv[2];
-
-writeFile(
-  join('docs', dir, 'index.html'),
-  pug.compileFile(join('src', dir, 'index.pug'))(),
-);
+const isProduction = process.env.NODE_ENV === 'production';
 
 const fuse = FuseBox.init({
   homeDir: 'src',
   output: join('docs', dir, '$name.js'),
-  sourceMaps: true,
+  sourceMaps: !isProduction,
   plugins: [
     BabelPlugin({
       config: {
-        sourceMaps: true,
+        sourceMaps: !isProduction,
         presets: [
           [
             'env',
             {
               targets: {
-                browsers: [
-                  'last 3 versions',
-                  'not < 0.25%',
-                  'not Android < 5',
-                ],
+                browsers: ['last 3 versions', 'not < 0.25%', 'not Android < 5'],
               },
-              useBuiltIns: 'usage',
+              useBuiltIns: true,
             },
           ],
-          [
-            'stage-0',
-          ],
+          ['stage-0'],
         ],
       },
     }),
+    WebIndexPlugin({
+      title: dir,
+      template: join('src', dir, 'index.html'),
+    }),
+    isProduction &&
+      QuantumPlugin({
+        target: 'browser',
+        bakeApiIntoBundle: 'bundle',
+        containedAPI: true,
+        treeshake: true,
+        uglify: { mangle: false },
+      }),
   ],
 });
 
-fuse
-  .bundle('bundle')
-  .instructions(`>${dir}/bundle.js`)
-  .hmr({ reload: true })
-  .watch();
-
-fuse.dev({
-  root: join('docs', dir),
-  httpServer: true,
-});
+if (isProduction) {
+  fuse.bundle('bundle').instructions(`>${dir}/bundle.js`);
+  fuse.dev({
+    root: join('docs', dir),
+    httpServer: true,
+  });
+} else {
+  fuse
+    .bundle('bundle')
+    .instructions(`>${dir}/bundle.js`)
+    .hmr({ reload: true })
+    .watch();
+  fuse.dev({
+    root: join('docs', dir),
+    httpServer: true,
+  });
+}
 
 fuse.run();
