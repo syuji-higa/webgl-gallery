8df1fd8d9d2806ffda3e610567b8eacb2d5771e5
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2018-07-30 15:32:32 +0900

    update return this & add getTime method

--- src/modules/utility/request-animation-framer.js
+++ src/modules/utility/request-animation-framer.js
@@ -10,28 +10,45 @@ class RequestAnimationFramer extends Singleton {
 
   /**
    * @param {Object} key
+   * @return {number} float[0,inf)
+   */
+  getTime(key) {
+    return this._animes.get(key).time;
+  }
+
+  /**
+   * @param {Object} key
    * @param {function} fn
    * @param {number} fps - int
    * @param {number} [interval] - int
+   * @return {Instance}
    */
   add(key, fn, fps, interval = 0) {
     this._animes.set(key, {
-      startTime: 0,
+      startTime: new Date().getTime(),
+      time: 0,
+      frameStartTime: 0,
       ms: fps === 'auto' ? 0 : Math.floor(1000 / fps),
+      time: 0,
       count: 0,
       interval: interval + 1,
       fn: fn,
     });
 
     if (!this._animateHandle) this._start();
+
+    return this;
   }
 
   /**
    * @param {Object} key
+   * @return {Instance}
    */
   remove(key) {
     this._animes.delete(key);
     if (!this._animes.size) this._stop();
+
+    return this;
   }
 
   _start() {
@@ -49,10 +66,12 @@ class RequestAnimationFramer extends Singleton {
 
     const _time = new Date().getTime();
 
-    this._animes.forEach(({ startTime, ms, count, interval, fn }, key) => {
-      this._animes.get(key).count++;
-      if (count % interval || ms > _time - startTime) return;
-      this._animes.get(key).startTime = _time;
+    this._animes.forEach(({ frameStartTime, ms, count, interval, fn }, key) => {
+      const _anime = this._animes.get(key);
+      _anime.count++;
+      _anime.time = _time - _anime.startTime;
+      if (count % interval || ms > _time - frameStartTime) return;
+      _anime.frameStartTime = _time;
       fn();
     });
   }
