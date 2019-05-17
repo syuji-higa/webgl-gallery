3d93a36ce2f27e6de0fbd93f623c8455165acc3f
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-05-17 19:43:25 +0900

    update debounce & throttle modules

--- src/modules/utility/debounce.js
+++ src/modules/utility/debounce.js
@@ -3,17 +3,31 @@
  */
 
 /**
- * @param {number} [interval] - int[0,inf)
+ * @param {Object} [opts]
+ * @param {number} [opts.interval]
+ * @param {boolean} [opts.isFirstRun]
  * @return {function(fn: function)}
  */
-export const debounce = function debounce(interval = 50) {
+export const debounce = (opts = {}) => {
+  const { interval, isFirstRun } = Object.assign(
+    { interval: 100, isFirstRun: false },
+    opts,
+  );
   let _timer = 0;
+  let _firstRun = true;
 
   return fn => {
+    if (isFirstRun && _firstRun) {
+      fn();
+      _firstRun = false;
+    }
     clearTimeout(_timer);
     _timer = setTimeout(() => {
       fn();
       _timer = 0;
+      if (isFirstRun) {
+        _firstRun = true;
+      }
     }, interval);
   };
 };
--- src/modules/utility/throttle.js
+++ src/modules/utility/throttle.js
@@ -3,16 +3,31 @@
  */
 
 /**
- * @param {number} [interval]
+ * @param {Object} [opts]
+ * @param {number} [opts.interval]
+ * @param {boolean} [opts.isLastRun]
  * @return {function(fn: function)}
  */
-export const throttle = (interval = 50) => {
-  let _lastTime = new Date().getTime() - interval;
+export const throttle = (opts = {}) => {
+  const { interval, isLastRun } = Object.assign(
+    { interval: 100, isLastRun: true },
+    opts,
+  );
 
-  return (fn) => {
-    if((_lastTime + interval) <= new Date().getTime()) {
+  let _lastTime = new Date().getTime() - interval;
+  let _timer = 0;
+
+  return fn => {
+    if (_lastTime + interval <= new Date().getTime()) {
       _lastTime = new Date().getTime();
       fn();
+    }
+    if (isLastRun) {
+      clearTimeout(_timer);
+      _timer = setTimeout(() => {
+        fn();
+        _timer = 0;
+      }, interval);
     }
   };
 };
